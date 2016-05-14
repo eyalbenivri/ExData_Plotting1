64ca01389bb864fddec493defa9971e84bb99758
Eyal Ben Ivri <eyalbenivri@gmail.com>
2016-05-14 10:14:38 +0300

    forgot to add legend to plot 3

--- Plot3.R
+++ Plot3.R
@@ -28,7 +28,7 @@ sdf <- subset(df, Date >= fromdate & Date <= todate)
 with(sdf, plot(Date + Time, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering"))
 with(sdf, lines(Date + Time, Sub_metering_2, col = "red"))
 with(sdf, lines(Date + Time, Sub_metering_3, col = "blue"))
-
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), lwd = c(1,1,1), col = c("black", "red", "blue"))
 
 #copy to png
 dev.copy(png, "./Plot3.png")
